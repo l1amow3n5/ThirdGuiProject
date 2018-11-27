9f7b98b4ae091737b3415d3fa16ce8df9fce9e64
Liam Owen <l1amow3n5@yahoo.com>
2018-11-27 12:22:14 -0700

    Made the Frame show up by completing the code in the Controller

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,7 +1,15 @@
 package gui.controller;
 
+import gui.view.GUIFrame;
+
 public class GUIController
 {
+	private GUIFrame appFrame;
+	
+	public GUIController()
+	{
+		appFrame = new GUIFrame(this);
+	}
 
 	public void start()
 	{
