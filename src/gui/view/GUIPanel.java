d3b9c3923a669fff4931ae5dadec259501829a1a
Liam Owen <l1amow3n5@yahoo.com>
2018-11-27 12:32:49 -0700

    added a JLabel and JButton to the Panel and also called the setupPanel Layout and Listeners methods

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -6,13 +6,37 @@ import gui.controller.GUIController;
 public class GUIPanel extends JPanel
 {
 	private GUIController appController;
+	private JLabel textLabel;
+	private JButton colorButton;
 	
 	public GUIPanel(GUIController appController)
 	{
 		super();
 		
 		this.appController = appController;
+		
+		this.textLabel = new JLabel("This is a color app!");
+		this.colorButton = new JButton("Click here to change color");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
 	
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 
 }
