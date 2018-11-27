6f37c9ad679f2458c3338a3ece433abb3a2d119d
Liam Owen <l1amow3n5@yahoo.com>
2018-11-27 12:04:34 -0700

    made the appController and the appPanel and started the setupFrame

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,6 +1,26 @@
 package gui.view;
 
-public class GUIFrame
-{
+import javax.swing.JFrame;
+import gui.controller.GUIController;
 
+public class GUIFrame extends JFrame
+{
+	private GUIController appController;
+	private GUIPanel appPanel;
+	
+	public GUIFrame(GUIController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new GUIPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		
+	}
+	
 }
